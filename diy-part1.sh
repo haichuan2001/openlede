a1f7040d692865de35771f6aaa8b5a135910a9e9
haichuan2001 <60537083+haichuan2001@users.noreply.github.com>
2022-01-28 14:48:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git AdGuardHome https://github.com/Twinzo1/AdGuardHome.git' feeds.conf.default
 sed -i '$a src-git IPTV https://github.com/haichuan2001/openwrt-iptvhelper.git' feeds.conf.default
-sed -i '$a src-git ZHIYONG https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-sed -i '$a src-git amule https://github.com/amule-project/amule.git' feeds.conf.default
+#sed -i '$a src-git ZHIYONG https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+#sed -i '$a src-git amule https://github.com/amule-project/amule.git' feeds.conf.default
